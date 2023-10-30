37921edddbb86117b269cd001a28310d03d040c0
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2023-10-30 10:20:24 +0000

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,64 @@
+// Common function to reset the dataLayer
+function resetDataLayer() {
+  // var gtm = window.google_tag_manager['GTM-M2RNBSN8'];
+  // gtm.dataLayer.reset();
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.length = 0;
+}
+
+
+function dlButton(elementId, ref, event, action) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.addEventListener('click', function() {
+      eventTime = new Date().getTime();
+      resetDataLayer();
+      dataLayer.push({
+     'elementId': elementId,
+     'ref': ref,
+     'event': event,
+     'action': action,
+     'process_start_time': pageLoadTime, 
+     'duration': (eventTime-pageLoadTime)/1000
+      });
+    });
+  }
+}
+
+function dlField(elementId, ref, event, category, object, action) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.addEventListener('focus', function() {
+      const eventTime = new Date().getTime();
+      resetDataLayer();
+      dataLayer.push({
+        'elementId': elementId,
+        'ref': ref,
+        'event': event,
+        'category':category,
+        'object': object,
+        'action': action,
+        'process_start_time': pageLoadTime, 
+        'duration': (eventTime - pageLoadTime) / 1000
+      });
+    });
+  }
+}
+
+ 
+function dlPage(elementId, ref, event, action) {
+  pageLoadTime = new Date().getTime();
+  // This event will trigger on page load
+  window.addEventListener('load', function() {
+    pageReadyTime = new Date().getTime();
+     resetDataLayer();
+     dataLayer.push({
+     'elementId': elementId,
+     'ref': ref,
+     'event': event,
+     'action': action,
+     'process_start_time': pageLoadTime,
+    'duration': (pageReadyTime-pageLoadTime)/1000
+    });
+  });
+}
