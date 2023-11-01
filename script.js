44e0be462cdfeb8a20d9309af04f8c7cf3a05fbb
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2023-11-01 08:55:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,24 @@ function redirectToCashier() {
     window.location.href = destinationPageURL;
 }
    
+function removeIdOnFocus() {
+  // Get all input fields
+  var inputFields = document.querySelectorAll("input");
+
+  // Attach the focus event listener to each input field
+  inputFields.forEach(function(input) {
+    input.addEventListener("focus", function() {
+      var elementWithId = this; // Store the element that received focus
+
+      // Add a delay of 1 second (you can adjust the delay as needed)
+      setTimeout(function() {
+        elementWithId.removeAttribute("id");
+      }, 500); // 1000 milliseconds = 1 second
+    });
+  });
+}
+
+// Call the function to set up the event listeners after the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  removeIdOnFocus();
+});
