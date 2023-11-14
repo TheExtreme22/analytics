d46c7e32abf5e109f38a376047d05b52db3a67d0
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2023-11-14 15:34:57 +0000

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,4 @@
- function removeGTMOnFocus() {
-  // Get all input fields
-  var inputFields = document.querySelectorAll("input");
-
-  // Attach the focus event listener to each input field
-  inputFields.forEach(function(input) {
-    input.addEventListener("focus", function() {
-      var elementWithId = this; // Store the element that received focus
-
-      // Add a delay of 1 second (you can adjust the delay as needed)
-      setTimeout(function() {
-        elementWithId.removeAttribute("gtm");
-      }, 500); // 1000 milliseconds = 1 second
-    });
-  });
-}
+ 
 
 function updateGTMOnFocus() {
   // Get all input fields
