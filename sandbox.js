ff1bab39dcc20f944ebd4847f428a8c2cdb99ca6
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2023-11-14 15:29:04 +0000

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -15,7 +15,36 @@
   });
 }
 
+function updateGTMOnFocus() {
+  // Get all input fields
+  var inputFields = document.querySelectorAll("input");
+
+  // Attach the focus event listener to each input field
+  inputFields.forEach(function(input) {
+    input.addEventListener("focus", function() {
+      var elementWithId = this; // Store the element that received focus
+
+      // Get the current value of the 'gtm' attribute
+      var gtmValue = elementWithId.getAttribute('gtm');
+
+      // If 'gtm' attribute exists, increase 'count' by 1 and update 'gtm' attribute
+      if (gtmValue !== null) {
+        var count = parseInt(elementWithId.getAttribute('count')) || 0;
+        count++;
+        elementWithId.setAttribute('count', count);
+      } else {
+        // If 'gtm' attribute doesn't exist, create it and set 'count' to 1
+        elementWithId.setAttribute('gtm', 'example');
+        elementWithId.setAttribute('count', 1);
+      }
+    });
+  });
+}
+
+
+
 // Call the function to set up the event listeners after the DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
-  removeGTMOnFocus();
+ //removeGTMOnFocus();
+ updateGTMOnFocus();
 });
