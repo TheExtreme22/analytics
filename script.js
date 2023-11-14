57ae236d7a5841f2d1caf3c57408feceaf734225
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2023-11-14 13:10:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,8 @@ function removeIdOnFocus() {
 document.addEventListener("DOMContentLoaded", function() {
   removeIdOnFocus();
 });
+
+// Call the function to set up the event listeners after the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  removeGTMOnFocus();
+});
