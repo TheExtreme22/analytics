91882e29507bd2204ec7cfdec8c3acea2802b7de
The Extreme <149299671+TheExtreme22@users.noreply.github.com>
2024-04-30 16:41:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 var currentStep = 1;
 
-// GET URL PARAMETER
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
+
 
 function nextStep(step) {
     document.getElementById(`step-${step}`).classList.remove('active');
